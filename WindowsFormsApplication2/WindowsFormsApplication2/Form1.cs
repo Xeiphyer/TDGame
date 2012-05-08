2eda479a95fb0474726121713c799a04c02f9976
Fil Hansen <mantis0@shaw.ca>
2012-05-08 17:23:07 -0600

    button is now a method

--- WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -14,8 +14,16 @@ namespace WindowsFormsApplication2
         public Form1()
         {
             InitializeComponent();
-            SButton();
-            
+            Text = "some shit here";//top of the window
+
+            Image image1 = new Bitmap("C:\\Users\\Phil\\TDGame\\WindowsFormsApplication2\\tower1.gif");//button image
+
+            SButton(450, 50, image1, "tower 1");
+            SButton(600, 50, image1, "tower 2");
+
+            Graphics grfx = CreateGraphics();
+            image(100, 100, image1, grfx);//not working
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -23,20 +31,19 @@ namespace WindowsFormsApplication2
 
         }
 
-        public void SButton()
+        public void SButton(int x, int y, Image img, String label)
         {
-            Text = "some shit here";//top of the window
             Button b = new Button();
 
             b.Parent = this;
-            b.Text = "Mutha' fuck'n tower 1";//label on the button
+            b.Text = label;//label on the button
+            b.TextAlign = ContentAlignment.BottomCenter;
 
-            Image img = new Bitmap("C:\\Users\\Phil\\TDGame\\WindowsFormsApplication2\\tower1.gif");//button image
             b.Image = new Bitmap(img);
             b.ImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
             b.Size = new System.Drawing.Size(img.Width, img.Height);
 
-            b.Location = new Point(500, 50);//x,y button location
+            b.Location = new Point(x, y);//x,y button location
             b.Click += new EventHandler(b_Click);
         }
 
@@ -47,10 +54,16 @@ namespace WindowsFormsApplication2
             string str = "Button clicked";
 
             grfx.DrawString(str, Font, new SolidBrush(ForeColor), ptText);
-            System.Threading.Thread.Sleep(250);
+            System.Threading.Thread.Sleep(500);
             grfx.FillRectangle(new SolidBrush(BackColor), new RectangleF(ptText, grfx.MeasureString(str, Font)));
 
             grfx.Dispose();
+        }
+
+        public static void image(int x, int y, Image img, Graphics grfx)//not working
+        {
+            grfx.DrawImage(img, x, y);
+            
         }
 
     }
