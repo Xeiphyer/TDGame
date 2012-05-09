24d60749c7952d5a9f0ec13bc46331c0278e36db
Fil Hansen <mantis0@shaw.ca>
2012-05-09 13:55:42 -0600

    awwww shit sprites

---
+++ WindowsGame1/WindowsGame1/WindowsGame1/sprite.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+
+
+namespace WindowsGame1
+{
+    class sprite
+    {
+        public Vector2 Position = new Vector2(0, 0);//The current position of the Sprite
+        private Texture2D mSpriteTexture;//The texture object used when drawing the sprite
+        
+        //Load the texture for the sprite using the Content Pipeline
+        public void LoadContent(ContentManager theContentManager, string theAssetName)
+        {
+            mSpriteTexture = theContentManager.Load<Texture2D>(theAssetName);
+        }
+
+        //Draw the sprite to the screen
+        public void Draw(SpriteBatch theSpriteBatch)
+        {
+            theSpriteBatch.Draw(mSpriteTexture, Position, Color.White);
+        }
+
+
+    }
+}
