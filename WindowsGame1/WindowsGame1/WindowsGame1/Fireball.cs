d3a845c7d4418a7b8a1c3e6976fdb60a222048a1
Fil Hansen <mantis0@shaw.ca>
2012-05-13 17:22:55 -0600

    attempted firball targeting, failed.

--- WindowsGame1/WindowsGame1/WindowsGame1/Fireball.cs
+++ WindowsGame1/WindowsGame1/WindowsGame1/Fireball.cs
@@ -16,7 +16,12 @@ namespace WindowsGame1
         Vector2 mStartPosition;
         Vector2 mSpeed;
         Vector2 mDirection;
-
+        const int MOVE_UP = -1;
+        const int MOVE_DOWN = 1;
+        const int MOVE_LEFT = -1;
+        const int MOVE_RIGHT = 1;
+        int X = 0;
+        int Y = 0;
 
         public Fireball()
         {
@@ -29,7 +34,7 @@ namespace WindowsGame1
             Scale = 0.3f;
         }
 
-        public void Update(GameTime theGameTime, Vector2 XY)
+        public void Update(GameTime theGameTime, Vector2 XY)//XY is from Pcrane
         {
             if (Vector2.Distance(mStartPosition, Position) > MAX_DISTANCE)
             {
@@ -38,6 +43,34 @@ namespace WindowsGame1
 
             if (Visible == true)
             {
+
+                  if (X > XY.X)
+                  {
+                      mSpeed.X = 100;
+                      mDirection.X = MOVE_LEFT;
+                      X = X + MOVE_LEFT;
+                  }
+
+                  if (X < XY.X)
+                  {
+                      mSpeed.X = 100;
+                      mDirection.X = MOVE_RIGHT;
+                      X = X + MOVE_RIGHT;
+                  }
+
+                  if (Y > XY.Y)
+                  {
+                      mSpeed.X = 100;
+                      mDirection.Y = MOVE_UP;
+                      Y = Y + MOVE_UP;
+                  }
+
+                  if (Y < XY.Y)
+                  {
+                      mSpeed.X = 100;
+                      mDirection.Y = MOVE_DOWN;
+                      Y = Y + MOVE_DOWN;
+                  }
                 base.Update(theGameTime, mSpeed, mDirection);
             }
         }
@@ -57,6 +90,8 @@ namespace WindowsGame1
             mSpeed = theSpeed;
             mDirection = theDirection;
             Visible = true;
+            int X = (int)mStartPosition.X;
+            int Y = (int)mStartPosition.Y;
         }
 
 
